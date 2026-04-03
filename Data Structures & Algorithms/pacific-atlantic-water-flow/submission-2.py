8fff4e9bc99d853c51104e4f2a15c18c22d334b8
Yash Chaudhary <124380387+YashChaudhary16@users.noreply.github.com>
2026-04-03 12:29:21 -0400

    Add: pacific-atlantic-water-flow - submission-2

---
+++ Data Structures & Algorithms/pacific-atlantic-water-flow/submission-2.py
@@ -0,0 +1,52 @@
+class Solution:
+    def pacificAtlantic(self, heights: List[List[int]]) -> List[List[int]]:
+        
+        rows, cols = len(heights), len(heights[0])
+
+        pacific = set()
+        atlantic = set()
+        directions = [[1,0],[-1,0],[0,1],[0,-1]]
+        res = []
+
+        for r in range(rows):
+            for c in range(cols):
+                if r == 0 or c == 0:
+                    pacific.add((r, c))
+                if r == (rows - 1) or c == (cols - 1):
+                    atlantic.add((r, c))
+        
+        pq = deque(pacific)
+        while pq:
+            (r, c) = pq.popleft()
+            pacific.add((r, c))
+
+            for dr, dc in directions:
+                row = dr + r
+                col = dc + c
+
+                if (row in range(rows) and
+                    col in range(cols) and
+                    heights[row][col] >= heights[r][c] and
+                    (row, col) not in pacific):
+                    pq.append((row, col))
+        
+        aq = deque(atlantic)
+        while aq:
+            (r, c) = aq.popleft()
+            atlantic.add((r, c))
+
+            for dr, dc in directions:
+                row = dr + r
+                col = dc + c
+
+                if (row in range(rows) and
+                    col in range(cols) and
+                    heights[row][col] >= heights[r][c] and
+                    (row, col) not in atlantic):
+                    aq.append((row, col))
+            
+        res = []
+        for (r, c) in atlantic:
+            if (r, c) in pacific:
+                res.append([r, c])
+        return res
